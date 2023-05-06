61560c63ec755444df291fd41f65c2b34323dfa9
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2023-05-06 12:21:14 +0200

    Move sources into src folder

--- build.py
+++ build.py
@@ -11,10 +11,6 @@ AUTHOR = "lgfrbcsgo"
 NAME = "Websocket Server"
 DESCRIPTION = "A single threaded, non blocking Websocket server for WoT mods which makes use of the `async` / `await`."
 
-SOURCES = [
-    "mod_websocket_server",
-]
-
 RELEASE_DEPENDENCIES = [
     "https://github.com/lgfrbcsgo/wot-async/releases/download/v0.1.3/lgfrbcsgo.async_0.1.3.wotmod",
     "https://github.com/lgfrbcsgo/wot-async-server/releases/download/v0.2.2/lgfrbcsgo.async-server_0.2.2.wotmod",
@@ -26,7 +22,7 @@ def clean():
     subprocess.check_call(["rm", "-rf", "dist"])
 
 
-@task()
+@task(clean)
 def wotmod():
     # clean dist directory
     subprocess.check_call(["rm", "-rf", "dist/wotmod"])
@@ -37,8 +33,7 @@ def wotmod():
     subprocess.check_call(["mkdir", "-p", source_dst])
 
     # copy sources
-    for source in SOURCES:
-        subprocess.check_call(["cp", "-r", source, source_dst])
+    subprocess.check_call(["cp", "-r", "src/.", source_dst])
 
     # compile sources
     subprocess.check_call(["python2.7", "-m", "compileall", source_dst])
